2bed216707daec676f6daeb90447df862a991831
Jinendra1316 <jinendrajain1316@gmail.com>
2025-07-07 19:24:35 +0530

    Data Validation

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from network_security.components.data_ingestion import DataIngestion
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
 import sys
-from network_security.entity.config_entity import DataIngestionConfig 
+from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig 
+from network_security.components.data_validation import DataValidation
+
+
 
 if __name__=='__main__':
     try:
@@ -13,7 +16,15 @@ if __name__=='__main__':
         
         logging.info("Initiate the data ingestion")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_Artifact=data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_Artifact)
+        
 
         
     except Exception as e:
