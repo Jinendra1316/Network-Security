6930484f069e606717c8d4ba6a59f53352276923
Jinendra1316 <jinendrajain1316@gmail.com>
2025-07-08 23:54:53 +0530

    Model Trainer and MLflow

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from network_security.entity.config_entity import DataIngestionConfig, DataValid
 from network_security.entity.config_entity import TrainingPipelineConfig 
 from network_security.components.data_validation import DataValidation
 from network_security.components.data_transformation import DataTransformation
-
+from network_security.components.model_trainer import ModelTrainer
+from network_security.entity.config_entity import ModelTrainerConfig
 
 if __name__=='__main__':
     try:
@@ -31,6 +32,17 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("Data Transformation completed")
+
+
+        logging.info("Model Training Started")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+
+
+
 
     except Exception as e:
            raise NetworkSecurityException(e,sys)
